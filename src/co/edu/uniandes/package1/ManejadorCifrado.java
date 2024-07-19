4ba9723438bc800dc89fe8e88ec5daedfe9f031f
Andrés Gómez <af.gomezg1@uniandes.edu.co>
2024-07-19 17:05:47 -0500

    implementar ManejadorCifrado

---
+++ src/co/edu/uniandes/package1/ManejadorCifrado.java
@@ -0,0 +1,39 @@
+package co.edu.uniandes.package1;
+
+import java.security.Key;
+
+import javax.crypto.Cipher;
+
+public class ManejadorCifrado {
+    public static byte[] cifrar(Key llave, String algoritmo, String texto) {
+        byte[] textoCifrado;
+
+        try {
+            Cipher cifrador = Cipher.getInstance(algoritmo);
+            byte[] textoClaro = texto.getBytes();
+
+            cifrador.init(Cipher.ENCRYPT_MODE, llave);
+            textoCifrado = cifrador.doFinal(textoClaro);
+
+            return textoCifrado;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String descifrar(Key llave, String algoritmo, byte[] textoCifrado) {
+        byte[] textoClaro;
+
+        try {
+            Cipher cifrador = Cipher.getInstance(algoritmo);
+            cifrador.init(Cipher.DECRYPT_MODE, llave);
+            textoClaro = cifrador.doFinal(textoCifrado);
+
+            return new String(textoClaro);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
